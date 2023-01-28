70539bc78681af1f35a79db2f1a8c4384a87ab32
Amfiteatr42 <108868537+Amfiteatr42@users.noreply.github.com>
2023-01-28 23:02:31 +0200

    adds global modal

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,22 @@
+import { Modal } from 'components/Modal/Modal';
+import { useState } from 'react';
 import { HeroContainer, MainTitle } from './HomePage.styled';
 
 const HomePage = () => {
+  const [isModalOpen, setIsModalOpen] = useState(true);
   return (
     <HeroContainer>
+      <button onClick={() => setIsModalOpen(true)}>OPEN</button>
       <MainTitle>Take good care of your small pets</MainTitle>
+      {isModalOpen && (
+        <Modal onCloseModal={() => setIsModalOpen(false)}>
+          <p>alksjdlkasjd</p>
+          <p>alksjdlkasjd</p>
+          <p>alksjdlkasjd</p>
+          <p>alksjdlkasjd</p>
+          <p>alksjdlkasjd</p>
+        </Modal>
+      )}
     </HeroContainer>
   );
 };

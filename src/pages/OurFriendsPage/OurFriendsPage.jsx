c764036e87cd17dba40b45ad1e0a57048d3a2c6a
Marianna Takovaya <mariannatakovaya@gmail.com>
2023-01-31 17:54:25 +0200

    1
    1

--- src/pages/OurFriendsPage/OurFriendsPage.jsx
+++ src/pages/OurFriendsPage/OurFriendsPage.jsx
@@ -2,11 +2,11 @@ import Friends from 'components/FriendsCard/Friends';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getOurFriends } from 'redux/ourFriend/ourFriendOperations';
-import { selectOurFriends } from 'redux/ourFriend/ourFriendOperations';
+import { selectOurFriends } from 'redux/ourFriend/ourFriendsSelectors';
 
 const OurFriendsPage = () => {
   const dispatch = useDispatch();
-  // const dataFriends = useSelector(selectOurFriends);
+  const dataFriends = useSelector(selectOurFriends);
   useEffect(() => {
     dispatch(getOurFriends());
   }, [dispatch]);

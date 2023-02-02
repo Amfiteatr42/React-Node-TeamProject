67514353555a7ede486a5cf55f49f0877dfaae7c
Volodymyr-Ogiychuk <volodymyr.ogiychuk@gmail.com>
2023-02-02 11:10:14 +0200

    Add correction of header positioning

--- src/components/SharedLayout/SharedLayout.styled.jsx
+++ src/components/SharedLayout/SharedLayout.styled.jsx
@@ -1,13 +1,15 @@
 import { default as styled } from 'styled-components';
 
 export const HeaderContainer = styled.header`
-    
+    margin: 0 auto;
     background-color: #FDF7F2;
      
 `;
 
 export const HeaderList = styled.ul`
+    position: relative;
     width: 320px;
+    margin: 0 auto;
     @media (min-width: 768px) and (max-width: 1279.99px) {
     width: 768px;
     };

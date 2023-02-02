0bc15895f7fdc47d154f82b07512dc7b590985c7
Volodymyr-Ogiychuk <volodymyr.ogiychuk@gmail.com>
2023-02-02 13:31:45 +0200

    Add chngs to Navigation isLoggedIn

--- src/components/Header/Navigation.jsx
+++ src/components/Header/Navigation.jsx
@@ -2,15 +2,15 @@ import Nav from './Nav';
 import AuthNav from './AuthNav';
 import UserNav from './UserNav';
 import { useState } from 'react';
-// import { useSelector } from 'react-redux';
-// import { getAuthIsLoggedIn } from "redux/selectors";
+import { useSelector } from 'react-redux';
+import { getAuthIsLoggedIn } from "../../redux/auth/selectors";
 import sprite from '../../assets/images/header/sprite.svg';
 import { NavigationList, NavigationItem, NavigationItemUser, UserNavBox, BurgerBtnItem, BurgerBtn, SvgBurger, SvgClose, CloseBtn, MobMenuContainer, UserAuthNavBox, MobMenuBox } from './Navigation.styled.jsx';
 
 export default function Navigation() {
     const [isMobMenuOpen, setIsMobMenuOpen] = useState(false)
-    const isLoggedIn = false;
-    // const isLoggedIn = useSelector(getAuthIsLoggedIn);
+    // const isLoggedIn = false;
+    const isLoggedIn = useSelector(getAuthIsLoggedIn);
 
     return (
         

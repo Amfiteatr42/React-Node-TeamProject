eeaee99ac6d88f919b555b10039a1f47ed8bbe22
Amfiteatr42 <108868537+Amfiteatr42@users.noreply.github.com>
2023-02-07 22:03:30 +0200

    fix warning

--- src/components/UserDataItem/User.jsx
+++ src/components/UserDataItem/User.jsx
@@ -7,10 +7,11 @@ import {
   Button,
   Svg,
   Item,
-  SvgUpdate,ErrorText
+  SvgUpdate,
+  ErrorText,
 } from './UserDataItem.styled';
 import { useOutClick } from '../../hooks/outClick';
-import { format, parse } from 'date-fns';
+//import { format, parse } from 'date-fns';
 import { useEffect } from 'react';
 const emailValid =
   /^([a-zA-Z0-9]{1}[\w\-.]{0,}[a-zA-Z0-9]{1})+@([\w-]+.)+[\w]{2,4}$/;
@@ -35,14 +36,13 @@ export const User = ({ label, name, user, active, setActive }) => {
   };
 
   useOutClick(wrapperRef, handelClick);
- 
 
   const handleChange = e => {
     const { name, value } = e.currentTarget;
 
     if (name === 'userName') {
       if (!value.match(nameValid)) {
-        console.log(!value.match(nameValid))
+        console.log(!value.match(nameValid));
         setDisable(true);
         setError('Name is not valid');
       } else if (error) {
@@ -88,18 +88,17 @@ export const User = ({ label, name, user, active, setActive }) => {
       setValue(value);
     }
   };
-   const handleDataFormat = date => {
-     if (!date?.length) return;
-    
-     const d = date?.split('-');
-  return d.join(''); 
+  const handleDataFormat = date => {
+    if (!date?.length) return;
+
+    const d = date?.split('-');
+    return d.join('');
   };
 
   const udateInput = name => () => {
-    const valid =
-      name === 'birthday' ?   handleDataFormat(value) : value;
+    const valid = name === 'birthday' ? handleDataFormat(value) : value;
     setActive('');
-    console.log(valid)
+    console.log(valid);
     dispatch(updateUserInfo({ [name]: valid }));
   };
 
@@ -109,30 +108,29 @@ export const User = ({ label, name, user, active, setActive }) => {
     <>
       <Item ref={active === name ? wrapperRef : null}>
         <Label>{label}</Label>
-     <Input
+        <Input
           active={active === name}
           disabled={active !== name}
-          type={ 'text'}
+          type={'text'}
           name={name}
           value={value}
           onChange={handleChange}
           minlength="2"
-          required/>
+          required
+        />
         {active === name ? (
           <Button disabled={disable} onClick={udateInput(name)}>
             {' '}
             <SvgUpdate />{' '}
           </Button>
         ) : (
-            <Button onClick={activeBtn(name)}>
+          <Button onClick={activeBtn(name)}>
             {' '}
             <Svg active={active} />
           </Button>
         )}
-
-       
       </Item>
-       {error && active === name ? <ErrorText>{error}</ErrorText> : null}
+      {error && active === name ? <ErrorText>{error}</ErrorText> : null}
     </>
   );
 };
